1e4ea7ca73eb4d39133a7a18ca9f3d0012560488
Douglas R. Miles <logicmoo@gmail.com>
2018-08-09 02:00:48 -0700

    need to have pack install nice and simple

---
+++ src/pakcsversion.pl
@@ -0,0 +1,10 @@
+:- module(pakcsversion,[compilerVersion/1, compilerMajorVersion/1, compilerMinorVersion/1, compilerRevisionVersion/1, buildVersion/1, buildDate/1, buildDir/1, pkgInstallDir/1, baseVersion/1]).
+compilerVersion('PAKCS2.0').
+compilerMajorVersion(2).
+compilerMinorVersion(0).
+compilerRevisionVersion(2).
+buildVersion(7).
+buildDate('2018-08-08').
+buildDir('/opt/logicmoo_workspace/packs_xtra/logicmoo_packages/prolog/pakcs').
+pkgInstallDir('').
+baseVersion('1.0.0').
